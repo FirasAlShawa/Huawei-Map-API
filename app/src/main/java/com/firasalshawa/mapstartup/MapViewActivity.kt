5241b5702c46773c70de162e77cc4b5b4ada675e
FIRAS AL SHAWA <f84155439@china.huawei.com>
2020-10-18 10:46:44 +0300

    setup2

--- app/src/main/java/com/firasalshawa/mapstartup/MapViewActivity.kt
+++ app/src/main/java/com/firasalshawa/mapstartup/MapViewActivity.kt
@@ -3,9 +3,11 @@ package com.firasalshawa.mapstartup
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MapViewActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
---
+++ app/src/main/java/com/firasalshawa/mapstartup/SupportMapFragmentActivity.kt
@@ -0,0 +1,11 @@
+package com.firasalshawa.mapstartup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SupportMapFragmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_support_map_fragment)
+    }
+}
